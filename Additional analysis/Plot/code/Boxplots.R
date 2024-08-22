ab9ef70c7293a47f3074736a6750b5fe371e04bf
ting459 <tzhang@mednet.ucla.edu>
2024-08-22 16:26:35 -0700

    updates

--- Additional analysis/Plot/code/Boxplots.R
+++ Additional analysis/Plot/code/Boxplots.R
@@ -1,36 +1,13 @@
 library(ggplot2)
 library(ggpubr)
-library(readxl)
-setwd("C:/Users/tingz/Downloads/COVID_Revision")
-Boxplot_input <- read_excel("C:/Users/tingz/Downloads/COVID_Revision/For violinplot area measurement.xlsx")
 
-Boxplot_input <- For_violinplot_area_measurement
-
+#load Boxplot_input, from "ExtendedFigs_1_2_3.rda"
 Boxplot_input$Group <- as.factor(Boxplot_input$Group)
 
+#Extended Figure 2
 ACE<- subset(Boxplot_input, Antibody == "ACE")
 tiff("ACE_2.tiff", units="in", width= 2.5, height=3, res=300)
 g <- ggplot(ACE, aes(x=Group, y=Area, fill = Group)) + 
-  geom_boxplot() +
-  geom_dotplot(binaxis='y', stackdir='center', position=position_dodge(1)) +
-  scale_fill_manual(values=c("#E69F00", "#56B4E9"))+
-  theme(legend.position = "none")
-g
-dev.off()
-
-Abeta <- subset(Boxplot_input, Antibody == "Abeta")
-tiff("Abeta.tiff", units="in", width= 2.5, height=3, res=300)
-g <- ggplot(Abeta, aes(x=Group, y=Area, fill = Group)) + 
-  geom_boxplot() +
-  geom_dotplot(binaxis='y', stackdir='center', position=position_dodge(1)) +
-  scale_fill_manual(values=c("#E69F00", "#56B4E9"))+
-  theme(legend.position = "none")
-g
-dev.off()
-
-NRP <- subset(Boxplot_input, Antibody == "NRP1")
-tiff("NRP.tiff", units="in", width= 2.5, height=3, res=300)
-g <- ggplot(NRP, aes(x=Group, y=Area, fill = Group)) + 
   geom_boxplot() +
   geom_dotplot(binaxis='y', stackdir='center', position=position_dodge(1)) +
   scale_fill_manual(values=c("#E69F00", "#56B4E9"))+
@@ -48,21 +25,15 @@ g <- ggplot(Furin, aes(x=Group, y=Area, fill = Group)) +
 g
 dev.off()
 
-Tau <- subset(Boxplot_input, Antibody == "Tau")
-tiff("Tau.tiff", units="in", width= 2.5, height=3, res=300)
-g <- ggplot(Tau, aes(x=Group, y=Area, fill = Group)) + 
+#Extended Figure 3
+NRP <- subset(Boxplot_input, Antibody == "NRP1")
+tiff("NRP.tiff", units="in", width= 2.5, height=3, res=300)
+g <- ggplot(NRP, aes(x=Group, y=Area, fill = Group)) + 
   geom_boxplot() +
   geom_dotplot(binaxis='y', stackdir='center', position=position_dodge(1)) +
   scale_fill_manual(values=c("#E69F00", "#56B4E9"))+
   theme(legend.position = "none")
 g
-dev.off()
-
-pdf("Tau.pdf", height = 2.5, width = 3)
-a <- ggplot(Tau, aes(x=Group, y=Area, fill = Group)) + 
-  geom_boxplot() +
-  geom_dotplot(binaxis='y', stackdir='center', position=position_dodge(1))
-a
 dev.off()
 
 Fibrin <- subset(Boxplot_input, Antibody == "Fibrin")
@@ -76,8 +47,29 @@ g+stat_compare_means(method = "t.test", label = "p", method.args = list(alternat
 g
 dev.off()
 
-nCounter_viral_probes <- Specific_3_probes_
+#Extended Figure 7
+Abeta <- subset(Boxplot_input, Antibody == "Abeta")
+tiff("Abeta.tiff", units="in", width= 2.5, height=3, res=300)
+g <- ggplot(Abeta, aes(x=Group, y=Area, fill = Group)) + 
+  geom_boxplot() +
+  geom_dotplot(binaxis='y', stackdir='center', position=position_dodge(1)) +
+  scale_fill_manual(values=c("#E69F00", "#56B4E9"))+
+  theme(legend.position = "none")
+g
+dev.off()
 
+Tau <- subset(Boxplot_input, Antibody == "Tau")
+tiff("Tau.tiff", units="in", width= 2.5, height=3, res=300)
+g <- ggplot(Tau, aes(x=Group, y=Area, fill = Group)) + 
+  geom_boxplot() +
+  geom_dotplot(binaxis='y', stackdir='center', position=position_dodge(1)) +
+  scale_fill_manual(values=c("#E69F00", "#56B4E9"))+
+  theme(legend.position = "none")
+g
+dev.off()
+
+#load nCounter_viral_probes from "ExtendedFigs_1_2_3.rda"
+#Extended Figure 1C
 tiff("Extended Fig 1C.tiff", units="in", width= 6, height= 4, res=300)
 g <- ggplot(nCounter_viral_probes, aes(x=Targets, y=Counts, fill = Disease)) + 
   geom_boxplot() +
@@ -87,4 +79,3 @@ g <- ggplot(nCounter_viral_probes, aes(x=Targets, y=Counts, fill = Disease)) +
 g+stat_compare_means(method = "t.test", label = "p", label.y = 62)
 dev.off()
 
-save(nCounter_viral_probes, Boxplot_input, file="ExtendedFigs.rda")
