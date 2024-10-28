7ed5e370ec2b48c8838e102fa38e9228578c1218
ting459 <tzhang@mednet.ucla.edu>
2024-10-28 11:36:01 -0700

    Update

--- Additional analysis/WGCNA/code/ConsensusFrontalGMPN.R
+++ Additional analysis/WGCNA/code/ConsensusFrontalGMPN.R
@@ -4,7 +4,7 @@ library(igraph)
 options(stringsAsFactors = FALSE);
 enableWGCNAThreads()
 
-setwd("./Fig2Sourcedata/WGCNA/concensusModule")
+setwd("./Fig2Sourcedata/WGCNA/consensusModule")
 
 #Load TOM from FrontalGM and PontineNuclei_linear_regression
 consensusTOM = pmin(TOM,TOM_PN)
@@ -18,43 +18,31 @@ sizeGrWindow(15,9)
 plot(geneTree, xlab="", sub="", main = "Gene clustering on Frontal12PN24",
      labels = FALSE, hang = 0.04)
 
-#load FrontalGMQ3_reg, PNQ3_reg
-multiExpr = list(list(data=t(FrontalGMQ3_reg)), list(data=PNQ3_reg))
-names(multiExpr) <- c("FrontalGM", "PNQ3")
-
-#Module selection#
-mColorh <- mLabelh <- colorLabels <- NULL
-for(minModuleSize in c(50, 100, 150)){
-  for(ds in c(2,4)){
-    for(dthresh in c(0.1, 0.2, 0.25)){
-      print("trying parameters:")
-      print(c(minModuleSize, dthresh,ds))
-      tree <- cutreeHybrid(dendro = geneTree, distM = as.matrix(dissTOM),
-                           deepSplit = ds, pamRespectsDendro = FALSE,pamStage = FALSE,
-                           minClusterSize = minModuleSize, cutHeight = 0.9999)
-      merged <- mergeCloseModules(exprData = multiExpr,colors = tree$labels,
-                                  cutHeight = dthresh)
-      mColorh <- cbind(mColorh, labels2colors(merged$colors))
-      mLabelh <- c(mLabelh, paste("DS=", ds, "mms=\n", minModuleSize, "dcor=", dthresh))
-    }
-  }
+########Module selection##########
+mColorh=NULL
+for (ds in 0:4){
+  tree = cutreeHybrid(dendro = geneTree, pamStage=FALSE,
+                      minClusterSize = 50, cutHeight = 0.9999,
+                      deepSplit = ds, distM = dissTOM)
+  mColorh=cbind(mColorh,labels2colors(tree$labels));
 }
-
-pdf("consensusTOMFrontalGM_PN.pdf", width = 12, height = 10)
-plotDendroAndColors(geneTree, mColorh, groupLabels = mLabelh,
-                    main = "Dendrogram with different module cutting thresholds",dendroLabels = FALSE,
-                    hang = 0.03, addGuide = TRUE, guideHang = 0.05)
+pdf("FrontalGM_PN_Module_choices_12_24_50.pdf", height=10,width=25);
+plotDendroAndColors(geneTree, mColorh, paste("dpSplt =",0:4), main = "FrontalGM12_PN24",dendroLabels=FALSE)
 dev.off()
 
-modules = mColorh[,4] # (Chosen based on plot)
+modules = mColorh[,5] #(Chosen based on plot below)
+pdf("FrontalGM_PN_Module.pdf", height=6, width = 18);
+plotDendroAndColors(geneTree, modules, main ="FrontalGM_PN", dendroLabels = FALSE)
+dev.off()
 
+#Extended Fig.3A
 tiff("FrontalGM_PN_Module.tiff", units="in", width=7, height=5, res=300)
 plotDendroAndColors(geneTree, modules, main ="FrontalGM_PN", dendroLabels = FALSE)
 dev.off()
 
-save(geneTree, mColorh, modules, tree, file = "FrontalGM and PN concensus_module_choice.RData")
+save(geneTree, mColorh, modules, tree, file = "FrontalGM and PN consensus_module_choice.RData")
 
-#consensusKME#
+########consensusKME############
 #Load FrontalGMQ3_reg, PNQ3_reg
 multiExpr = list(list(data=t(FrontalGMQ3_reg)), list(data=PNQ3_reg))
 names(multiExpr) <- c("FrontalGM", "PNQ3")
@@ -74,7 +62,7 @@ consensus_kM <- consensusKME(
   excludeGrey = TRUE, 
   greyLabel = if (is.numeric(moduleLabels)) 0 else "grey")
 consensus_kM<- as.data.frame(consensus_kM)
-consensus_kM_s <- consensus_kM[, grep("consensus", colnames(concensus_kM))]
+consensus_kM_s <- consensus_kM[, grep("consensus", colnames(consensus_kM))]
 consensus_kM_s$module <- modules
 consensus_kM_s$Gene <- rownames(consensus_kM_s)
 write.csv(consensus_kM_s, file="consensuskM_table.csv")
@@ -83,21 +71,21 @@ individual_kM <- consensus_kM[, grep("Z.", colnames(consensus_kM))]
 individual_FrontalkM <-consensus_kM[, grep("Frontal", colnames(consensus_kM))]
 individual_PNKM <-consensus_kM[, grep("PNQ3", colnames(consensus_kM))]
 write.csv(individual_FrontalkM, file="kM_FrontalGM.csv")
-write.csv(individual_PNkM, file="kM_PN.csv")
-save(consensus_kM,consensus_kM_s, individual_FrontalkM, individual_PNkM, file="consensus_kM.RData")
+write.csv(individual_PNKM, file="kM_PN.csv")
+save(consensus_kM,consensus_kM_s, individual_FrontalkM, individual_PNKM, file="consensus_kM.RData")
 
 
-#Differential module expression analysis#
-#Use the concensus modules to calculate eigengenes#
+#############Differential module expression analysis################
+#Use the consensus modules to calculate eigengenes#
 MEList_Fc = moduleEigengenes(t(FrontalGMQ3_reg), colors = modules)
 MEs_Fc = MEList_Fc$eigengenes
 
 MEList_Pc = moduleEigengenes(PNQ3_reg, colors = modules)
 MEs_Pc = MEList_Pc$eigengenes
 
-save(geneTree, mColorh, modules, tree,FrontalGMQ3_reg, target_Frontal, PNQ3_reg, target_PN, MEList_Fc, MEs_Fc, MEList_Pc, MEs_Pc, file = "FrontalGM and PN concensus_module_choice.RData")
+save(geneTree, mColorh, modules, tree,FrontalGMQ3_reg, target_Frontal, PNQ3_reg, target_PN, MEList_Fc, MEs_Fc, MEList_Pc, MEs_Pc, file = "FrontalGM and PN consensus_module_choice.RData")
 
-#FrontalGM
+#########FrontalGM##########
 New_Fc<- cbind(target_Frontal, MEs_Fc)
 ME_names_Fc <- colnames(MEs_Fc)
 
@@ -186,9 +174,9 @@ Res_filtered_Fc$MEColors = rownames(Res_filtered_Fc)
 Res_filtered_grey_Fc <- Res_filtered_Fc[!Res_filtered_Fc$MEColors %in% c("MEgrey"),]
 Res_filtered_grey_Fc$Area <- c("FrontalGM")
 
-save(Res_filtered_Fc,Res_filtered_grey_Fc, lm_results_Fc, ME_names_Fc, New_Fc, file = "DifferentialFrontalGMmodules_Concensus.RData")
+save(Res_filtered_Fc,Res_filtered_grey_Fc, lm_results_Fc, ME_names_Fc, New_Fc, file = "DifferentialFrontalGMmodules_consensus.RData")
 
-#Pons
+#########Pons#############
 New_Pc<- cbind(target_PN, MEs_Pc)
 ME_names_Pc <- colnames(MEs_Pc)
 
@@ -290,7 +278,7 @@ Res_filtered_Pc$MEColors = rownames(Res_filtered_Pc)
 Res_filtered_grey_Pc <- Res_filtered_Pc[!Res_filtered_Pc$MEColors %in% c("MEgrey"),]
 Res_filtered_grey_Pc$Area <- c("PontineNuclei")
 
-save(Res_filtered_Pc,Res_filtered_grey_Pc, lm_results_Pc, ME_names_Pc, New_Pc, file = "DifferentialPNmodules_Concensus.RData")
+save(Res_filtered_Pc,Res_filtered_grey_Pc, lm_results_Pc, ME_names_Pc, New_Pc, file = "DifferentialPNmodules_consensus.RData")
 
 write.csv(Res_Fc,Res_Pc, file = "FrontalGMPN_consensusmodules_DE_lm.csv")
 
